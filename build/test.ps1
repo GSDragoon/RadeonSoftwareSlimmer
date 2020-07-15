44c1220eb6d7ede615b1528f0773a6e1e94d7558
GSDragoon <gsdragoon@outlook.com>
2020-07-14 22:38:24 -0400

    Testing using getversion in a script

--- build/test.ps1
+++ build/test.ps1
@@ -7,3 +7,11 @@
 # Run the tests in the test project
 # This will automatically do both .net core and .net framework versions
 dotnet test .\test\RadeonSoftwareSlimmer.Test\RadeonSoftwareSlimmer.Test.csproj
+
+
+$param1=$args[0]
+Write-Host "Parameter: $param1"
+
+Write-Host "GitHub Value: ${{ steps.gitversion.outputs.semVer }}""
+
+Write-Host "Environmental Variable: $Env:steps.gitversion.outputs.semVer"
