31a6fb7067860b7be686fa7c009a219fbcb087e3
Greg Seaton <37422356+GSDragoon@users.noreply.github.com>
2020-07-16 00:01:20 -0400

    Merge build-process branch

---
+++ build/publish.ps1
@@ -0,0 +1,19 @@
+﻿# Builds the software and creates release artifacts
+
+# Get the version from GetVersion environmental variable
+$version = $env:GitVersion_SemVer
+
+# Build and create artifacts
+dotnet publish --configuration Release --framework netcoreapp3.1 --self-contained false --force --output .\publish\netcoreapp3.1 .\src\RadeonSoftwareSlimmer\RadeonSoftwareSlimmer.csproj -p:Version=$version
+dotnet publish --configuration Release  --framework net48 --force --output .\publish\net48 .\src\RadeonSoftwareSlimmer\RadeonSoftwareSlimmer.csproj -p:Version=$version
+
+# Output ProductVersion
+$productVersion = (Get-Item .\publish\netcoreapp3.1\RadeonSoftwareSlimmer.exe).VersionInfo.ProductVersion
+Write-Host "ProductVersion: $productVersion"
+
+# Archive the artifacts
+Compress-Archive -Path .\publish\netcoreapp3.1\* -DestinationPath ".\publish\RadeonSoftwareSlimmer_${version}_netcoreapp31.zip"
+Compress-Archive -Path .\publish\net48\* -DestinationPath ".\publish\RadeonSoftwareSlimmer_${version}_net48.zip"
+
+# Output the version
+Write-Host "Published: $version"
---
+++ test/RadeonSoftwareSlimmer.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+// Placeholder test to get the build process in place
+
+namespace RadeonSoftwareSlimmer.Test
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
