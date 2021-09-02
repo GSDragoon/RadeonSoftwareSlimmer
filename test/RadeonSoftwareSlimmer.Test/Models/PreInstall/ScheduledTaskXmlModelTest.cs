4603abfecf5d219f086e001f50954497fa0619d7
GSDragoon <gsdragoon@outlook.com>
2021-09-01 23:57:10 -0400

    scheduled task xml test

--- test/RadeonSoftwareSlimmer.Test/Models/PreInstall/ScheduledTaskXmlModelTest.cs
+++ test/RadeonSoftwareSlimmer.Test/Models/PreInstall/ScheduledTaskXmlModelTest.cs
@@ -1,14 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using RadeonSoftwareSlimmer.Models.PreInstall;
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
 
 namespace RadeonSoftwareSlimmer.Test.Models.PreInstall
 {
     [TestFixture]
-    class ScheduledTaskXmlModelTest
+    public class ScheduledTaskXmlModelTest
     {
+        [Test]
+        public void GetFile_ReturnsFile()
+        {
+            string filePath = @"C:\Some\path\To\A\file.xml";
+            MockFileSystem fileSystem = new MockFileSystem();
+            fileSystem.AddFile(filePath, new MockFileData(string.Empty));
+            IFileInfo originalFile = fileSystem.FileInfo.FromFileName(filePath);
+            ScheduledTaskXmlModel scheduledTask = new ScheduledTaskXmlModel(originalFile);
+
+            IFileInfo returnFile = scheduledTask.GetFile();
+
+            Assert.That(returnFile, Is.Not.Null);
+            Assert.That(returnFile, Is.EqualTo(originalFile));
+        }
     }
 }
