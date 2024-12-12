28ea8fbd1c3ca70005afdf808b142fbb25a8068a
Greg Seaton <gsdragoon@outlook.com>
2024-12-12 01:19:26 -0500

    fix typo

--- build/test.ps1
+++ build/test.ps1
@@ -10,6 +10,6 @@ $ProgressPreference = 'SilentlyContinue'
 # This will automatically do both .net core and .net framework versions
 Write-Output '***** Testing solution...'
 #dotnet test .\test\RadeonSoftwareSlimmer.Test\RadeonSoftwareSlimmer.Test.csproj
-dotnet test --no-build --configuration Release --framework net9.0-windows --results-directory "${testResultsDir}"
-dotnet test --no-build --configuration Release --framework net8.0-windows --results-directory "${testResultsDir}"
-dotnet test --no-build --configuration Release --framework net48 --results-directory "${testResultsDir}"
+dotnet test --no-build --configuration Release --framework net9.0-windows
+dotnet test --no-build --configuration Release --framework net8.0-windows
+dotnet test --no-build --configuration Release --framework net48
