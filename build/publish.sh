5f2990d3d14982826edc0f311db55b0e16c49670
Greg Seaton <gsdragoon@outlook.com>
2026-04-15 01:35:36 -0400

    local dev and testing on linux

---
+++ build/publish.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eu
+
+
+version=BUILD_VERSION
+echo"Version: ${version}"
+
+
+echo '***** Publishing Application...'
+dotnet publish ./src/RadeonSoftwareSlimmer/RadeonSoftwareSlimmer.csproj --force --configuration Release -p:Version=$version --framework net9.0-windows
+dotnet publish ./src/RadeonSoftwareSlimmer/RadeonSoftwareSlimmer.csproj --force --configuration Release -p:Version=$version --framework net8.0-windows
+#dotnet publish ./src/RadeonSoftwareSlimmer/RadeonSoftwareSlimmer.csproj --force --configuration Release -p:Version=$version --framework net48
+echo '***** Done Publishing Application...'
+
+
+echo '***** Archiving Application...'
+publishSrcDir='./artifacts/publish/RadeonSoftwareSlimmer'
+publishDestDir='./artifacts/publish'
+
+zip -r "${publishDestDir}/RadeonSoftwareSlimmer_${version}_net90.zip" "${publishSrcDir}/release_net9.0-windows_win-x64/*"
+zip -r "${publishDestDir}/RadeonSoftwareSlimmer_${version}_net80.zip" "${publishSrcDir}/release_net8.0-windows_win-x64/*"
+#zip -r "${publishDestDir}/RadeonSoftwareSlimmer_${version}_net48.zip" "${publishSrcDir}/release_net48_win-x64/*"
+echo '***** Done Archiving Application...'
+
+
+echo '***** Archive Hashes (SHA256):'
+sha256sum "${publishDestDir}\RadeonSoftwareSlimmer_*_net*.zip"
---
+++ local-ci.sh
@@ -0,0 +1,23 @@
+﻿#!/bin/bash
+
+set -eu
+
+
+export BUILD_VERSION='1.2.3-localci'
+
+echo '***** Recreating artifacts directory...'
+artifactPath='./artifacts'
+echo $artifactPath
+if [[ -d "${artifactPath}" ]]; then
+  rm --recursive --force "${artifactPath:?}/*"
+fi
+
+
+bash ./build/build.sh
+
+bash ./build/test.sh
+
+bash ./build/publish.sh
+
+
+echo '***** Done!'
